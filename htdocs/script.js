977e98a0dfe3c5330467ab801dfe704ba62c8700
Mike Davidson <enfarious@gmail.com>
2025-07-25 08:12:53 -0400

    Close now

--- htdocs/script.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.book-toggle').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const chapters = btn.nextElementSibling;
-        chapters.style.display = chapters.style.display === 'block' ? 'none' : 'block';
-    });
-});
